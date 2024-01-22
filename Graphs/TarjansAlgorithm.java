cbc6de6aae158d41d4fcc8eef8ecd4e33fd61b20
Atharva Sawleshwarkar <84561813+AtharvaMS@users.noreply.github.com>
2024-01-22 20:18:10 +0530

    Trajan's Algorithm
    used to find Bridge in Graph.
    Concept of time of discovery based

---
+++ Graphs/TarjansAlgorithm.java
@@ -0,0 +1,87 @@
+package Graphs;
+
+import java.util.ArrayList;
+
+public class TarjansAlgorithm {
+    static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int src, int dest){
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0; i<graph.length;i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+
+        graph[3].add(new Edge(3, 0));
+        graph[3].add(new Edge(3, 4));
+        // graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 3));
+        // graph[4].add(new Edge(4, 5));
+
+        // graph[5].add(new Edge(5, 3));
+        // graph[5].add(new Edge(5, 4));
+    }
+
+    public static void findBridgeTrajansAlgo(ArrayList<Edge> graph[], int v){
+        int discovery_time[] = new int[v];
+        int lowest_discovery_time[] = new int[v];
+        boolean vis[] = new boolean[v];
+        int time =0;
+
+        for(int i=0; i<v; i++){
+            if(!vis[i]){
+                dfs(graph, i, -1, discovery_time, lowest_discovery_time, vis, time);
+            }
+        }
+    }
+
+    private static void dfs(ArrayList<Edge>[] graph, int curr, int par, int[] discovery_time, int[] lowest_discovery_time,
+            boolean[] vis, int time) {
+        vis[curr] = true;
+        discovery_time[curr] = lowest_discovery_time[curr] = ++time;
+
+        for(int i=0; i<graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            int neighbour = e.dest;
+
+            if(neighbour == par){
+                continue;
+            }else if (!vis[neighbour]) {
+                dfs(graph, neighbour, curr, discovery_time, lowest_discovery_time, vis, time);
+                lowest_discovery_time[curr] = Math.min(lowest_discovery_time[curr], lowest_discovery_time[neighbour]);
+                if(discovery_time[curr] < lowest_discovery_time[neighbour]){
+                    System.out.println("Bridge Edge " + curr + "----> " + neighbour);
+                }
+            }else{
+                lowest_discovery_time[curr] = Math.min(lowest_discovery_time[curr], discovery_time[neighbour]);
+            }
+        }
+    }   
+
+    public static void main(String[] args) {
+        int v =6;
+        ArrayList<Edge> graph[] = new ArrayList[v];
+        createGraph(graph);
+        findBridgeTrajansAlgo(graph, v);
+
+
+    }
+    
+}
